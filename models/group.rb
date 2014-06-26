1f9296ef04466fa45181eedfb99078365c3ad8de
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2014-06-26 07:27:53 -0500

    Fix bug with user groups

--- models/group.rb
+++ models/group.rb
@@ -4,5 +4,5 @@ class Group
   belongs_to :owner, :class_name => 'User', :inverse_of => :groups
   has_many :wishes, :class_name => 'Item', :inverse_of => :group
   has_and_belongs_to_many :friends, :class_name => 'User',
-    :inverse_of => :in_groups
+    :inverse_of => nil
 end
--- models/user.rb
+++ models/user.rb
@@ -3,7 +3,5 @@ class User
   field :fbid, type: String
   embeds_many :events
   has_many :groups, :inverse_of => :owner
-  has_and_belongs_to_many :in_groups, :class_name => 'Group',
-    :inverse_of => :friends
   has_many :gifts, :class_name => 'Item', :inverse_of => :picker
 end
